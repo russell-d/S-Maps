1a59732d27a2d10fe3433236cdb0e3111af3b996
Ubuntu <ubuntu@ip-172-31-88-154.ec2.internal>
2019-08-09 16:24:40 +0000

    hello

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import webapp2
 import jinja2
 import os
-from model import Sports
+from model import Sports3
 from google.appengine.api import users
+import logging
+from random import shuffle
 
 
 the_jinja_env = jinja2.Environment(
@@ -12,31 +14,46 @@ the_jinja_env = jinja2.Environment(
 
 class HomeHandler(webapp2.RequestHandler):
     def get(self):  # for a get request
+    
+        
         home_template = the_jinja_env.get_template('templates/home.html')
         self.response.write(home_template.render())
-
+    
     def post(self):
-        sports = Sports(
-            line1=self.request.get('user-first-ln'), 
-            line2=self.request.get('user-second-ln'), 
-            sports_type=self.request.get('sports-type')
-        )
-        sports_key = sports.put()
-        self.response.write("Sports created: " + str(sports_key) + "<br>")
+        
+        # self.response.write("%s %s %s %s %s" % (Sports2.name, Sports2.park, Sports2.time, Sports2.date, Sports2.sports_type))
+        
+        sports = Sports3(
+            name = self.request.get('user-first-ln'),
+            park = self.request.get('user-second-ln'),
+            time = self.request.get('user-third-ln'),
+            date = self.request.get('user-fourth-ln'),
+            sports_type = self.request.get('sports-type')
+            )
+        
+        sports.put()
+        self.response.write("Sports created: "  + "<br>")
         self.response.write("<a href='/Finder'>Sports</a>")
-
+       
 class AllSportsHandler(webapp2.RequestHandler):
+    
     def get(self):
+        all_sports = Sports3.query().fetch()
         
-        all_sports = Sports.query().fetch()
-        
-        the_variable_dict = {
-            "all_sports": all_sports
+        sports_dict = {
+            'all_sports' : all_sports
         }
         
-        Finder_template = the_jinja_env.get_template('templates/finder.html')
-        self.response.write(Finder_template.render(the_variable_dict))
+        latLong = "{lat: 36.598339, lng : -121.896431}"
+        myDict = {
+            'key': unicode(latLong,"utf-8"),
+            'all_sports' : all_sports
+        }
         
+        finder_template =the_jinja_env.get_template('templates/finder.html')
+        self.response.write(finder_template.render(myDict))
+
+    
 app = webapp2.WSGIApplication([
     ('/', HomeHandler),
     ('/Finder', AllSportsHandler),
--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 from google.appengine.ext import ndb
 
-class Sports(ndb.Model):
-    line1 = ndb.StringProperty(required=True)
-    line2 = ndb.IntegerProperty(required=True)
-    sports_type = ndb.StringProperty(required=False)
+class Sports3(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    park = ndb.StringProperty(required=True)
+    time = ndb.StringProperty(required=True)
+    date = ndb.StringProperty(required=True)
+    sports_type = ndb.StringProperty(required=False)    
